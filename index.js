2d9455d082ce6acf594e224d279c446c260eefcf
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-03-19 16:55:50 -0500

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const api = require('./api/v1');
@@ -8,6 +9,19 @@ const {
   errorHandler,
   boomErrorHandler,
 } = require('./api/v1/middlewares/error.handler');
+app.use(express.json());
+//cors
+const whiteList = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Access denied (cors)'));
+    }
+  },
+};
+app.use(cors(options));
 // Routes
 app.use('/api', api);
 app.use('/api/1', api);
