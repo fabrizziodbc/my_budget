21407dc46dbdc520f2398331829ac17f7e7d662a
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-03-20 13:58:05 -0500

    add operation validation schema

---
+++ api/v1/operation/schema.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+
+const id = Joi.string().uuid();
+const concept = Joi.string();
+const amount = Joi.number();
+const type = Joi.string().valid('expense', 'income');
+const date = Joi.date().max('now');
+
+const createOperationSchema = Joi.object({
+  concept: concept.required(),
+  amount: amount.required(),
+  type: type.required(),
+  date: date.required(),
+});
+const updateOperationSchema = Joi.object({
+  concept: concept,
+  amount: amount,
+  date: date,
+});
+const getOperationSchema = Joi.object({
+  id: id.required(),
+});
+
+module.exports = {
+  createOperationSchema,
+  getOperationSchema,
+  updateOperationSchema,
+};
