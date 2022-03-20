8f5f31ed5e456ba130030dc5a010a7e3618aa707
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-03-20 15:14:17 -0500

    add balance routes

--- api/v1/index.js
+++ api/v1/index.js
@@ -2,12 +2,13 @@
 const express = require('express');
 
 const userRouter = require('./user/routes');
+const balanceRouter = require('./balance/routes');
 const operationRouter = require('./operation/routes');
-
 
 const router = express.Router();
 
 router.use('/user', userRouter);
+router.use('/balance', balanceRouter);
 router.use('/operation', operationRouter);
 
 module.exports = router;
