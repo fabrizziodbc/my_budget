747ae10c52e713737ace1cb247ba8d26ba1725b6
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-03-23 14:59:04 -0500

    add config connection url

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,11 +4,17 @@ const { config } = require('../config/config');
 
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const options = {
+  dialect: 'postgres',
+  logging: config.isProd ? false : true,
+};
+if (config.isProd) {
+  options.ssl = {
+    rejectUnauthorized: false,
+  };
+}
 
-const sequelize = new Sequelize(URI, { dialect: 'postgres', logging: true });
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
 
